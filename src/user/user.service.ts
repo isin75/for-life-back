1acbefdcc7e1504d6ca711db8c461eab9d8fa868
isin75 <jon.s124524@gmail.com>
2024-07-24 20:59:58 +0300

    feat: add user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common'
 import { hash } from 'argon2'
 import { PrismaService } from 'prisma/prisma.service'
 import { AuthDto } from 'src/auth/dto/auth.dto'
+import { UserDto } from './dto/user.dto'
+import { startOfDay, subDays } from 'date-fns'
 
 @Injectable()
 export class UserService {
@@ -24,6 +26,55 @@ export class UserService {
       }
     })
   }
+
+  async getProfile(id: string) {
+    const profile = await this.getById(id)
+
+    const totalTasks = profile.tasks.length
+    const completedTasks = await this.prisma.task.count({
+      //для продакшена нужно переписать на прокидывания с тасков
+      where: {
+        userId: id,
+        isCompleted: true
+      }
+    })
+
+    const todayStart = startOfDay(new Date())
+    const weekStart = startOfDay(subDays(new Date(), 7))
+
+    const todayTasks = await this.prisma.task.count({
+      //для продакшена нужно переписать на прокидывания с тасков
+      where: {
+        userId: id,
+        createdAt: {
+          gte: todayStart.toISOString()
+        }
+      }
+    })
+
+    const weekTasks = await this.prisma.task.count({
+      //для продакшена нужно переписать на прокидывания с тасков
+      where: {
+        userId: id,
+        createdAt: {
+          gte: weekStart.toISOString()
+        }
+      }
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...rest } = profile
+    return {
+      user: rest,
+      statistics: [
+        { label: 'Total', value: totalTasks },
+        { label: 'Completed tasks', value: completedTasks },
+        { label: 'Today tasks', value: todayTasks },
+        { label: 'Week tasks', value: weekTasks }
+      ]
+    }
+  }
+
   async create(dto: AuthDto) {
     const user = {
       email: dto.email,
@@ -32,6 +83,25 @@ export class UserService {
     }
     return this.prisma.user.create({
       data: user
+    })
+  }
+
+  async update(id: string, dto: UserDto) {
+    let data = dto
+
+    if (dto.password) {
+      data = { ...dto, password: await hash(dto.password) }
+    }
+
+    return this.prisma.user.update({
+      where: {
+        id
+      },
+      data,
+      select: {
+        name: true,
+        email: true
+      }
     })
   }
 }
