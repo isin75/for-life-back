350fde55f163ccf0d9e93eb5cf955d31829c72b9
isin75 <jon.s124524@gmail.com>
2024-07-31 13:16:19 +0300

    feat: add pomodoro

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
 import { TaskModule } from './task/task.module'
 import { TimeBlockModule } from './time-block/time-block.module'
+import { PomodoroModule } from './pomodoro/pomodoro.module'
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { TimeBlockModule } from './time-block/time-block.module'
     AuthModule,
     UserModule,
     TaskModule,
-    TimeBlockModule
+    TimeBlockModule,
+    PomodoroModule
   ],
   controllers: [AppController],
   providers: [AppService]
