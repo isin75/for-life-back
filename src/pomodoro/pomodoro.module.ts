79b59d8b7ec31284aa75cec37b664a3bdc3b5aeb
isin75 <jon.s124524@gmail.com>
2024-08-29 19:10:42 +0300

    vercel deploy fix

--- src/pomodoro/pomodoro.module.ts
+++ src/pomodoro/pomodoro.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { PomodoroController } from './pomodoro.controller'
 import { PomodoroService } from './pomodoro.service'
-import { PrismaService } from 'prisma/prisma.service'
+import { PrismaService } from 'src/prisma.service'
 
 @Module({
   controllers: [PomodoroController],
--- src/time-block/time-block.module.ts
+++ src/time-block/time-block.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { TimeBlockController } from './time-block.controller'
 import { TimeBlockService } from './time-block.service'
-import { PrismaService } from 'prisma/prisma.service'
+import { PrismaService } from 'src/prisma.service'
 
 @Module({
   controllers: [TimeBlockController],
