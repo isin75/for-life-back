4fe37c966ad57451be819c296ec20dbaa2b3659a
isin75 <jon.s124524@gmail.com>
2024-08-30 16:49:42 +0300

    vercel deploy fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { UserModule } from 'src/user/user.module'
+import { UserModule } from '../user/user.module'
 import { JwtStrategy } from './jwt.strategy'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
